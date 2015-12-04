dda8df143dc24b197c775c017013980002e74923
felippemr <raposo.felippe@gmail.com>
2015-12-04 12:07:36 -0200

    Improve append and increment

--- command_handler/append.py
+++ command_handler/append.py
@@ -5,14 +5,19 @@ from settings import DATABASE_DICT
 def handle_append(key, value):
     """Return a tuple containing True if the key's value could be appended to
     and the message to send back to the client."""
-    return_value = exists, list_value = handle_get(key)
-    if not exists:
-        return return_value
-    elif not isinstance(list_value, list):
-        return (
-            False,
-            'ERROR: Key [{}] contains non-list value ([{}])'.format(key, value)
-        )
-    else:
+    exists, list_value = handle_get(key)
+
+    try:
+        DATABASE_DICT[key]
         DATABASE_DICT[key].append(value)
-        return (True, 'Key [{}] had value [{}] appended'.format(key, value))
+    except KeyError:
+        operation_status = False
+        return_msg = 'Key [{}] does not exists'.format(key)
+    except AttributeError:
+        operation_status = False
+        return_msg = 'ERROR: Key [{}] contains non-list value ([{}])'.format(key, value)
+    else:
+        operation_status = True
+        return_msg = 'Key [{}] had value [{}] appended'.format(key, value)
+
+    return operation_status, return_msg
--- command_handler/increment.py
+++ command_handler/increment.py
@@ -5,14 +5,19 @@ from settings import DATABASE_DICT
 def handle_increment(key):
     """Return a tuple containing True if the key's value could be incremented
     and the message to send back to the client."""
-    return_value = exists, value = handle_get(key)
-    if not exists:
-        return return_value
-    elif not isinstance(value, int):
-        return (
-            False,
-            'ERROR: Key [{}] contains non-int value ([{}])'.format(key, value)
-        )
-    else:
+    exists, value = handle_get(key)
+
+    try:
+        DATABASE_DICT[key]
         DATABASE_DICT[key] = value + 1
-        return (True, 'Key [{}] incremented'.format(key))
+    except KeyError:
+        operation_status = False
+        return_msg = 'ERROR: Key [{}] does not exists'.format(key)
+    except TypeError:
+        operation_status = False
+        return_msg = 'ERROR: Key [{}] contains non-int value ([{}])'.format(key, value)
+    else:
+        operation_status = True
+        return_msg = 'Key [{}] incremented'.format(key)
+
+    return operation_status, return_msg
